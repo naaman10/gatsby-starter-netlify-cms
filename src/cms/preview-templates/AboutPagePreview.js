335bd670a7d23966cc2a052d3bd6ee82c8f3d196
Naaman <naamanh10@gmail.com>
2020-05-07 22:09:29 +0100

    Update AboutPagePreview.js

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -4,10 +4,11 @@ import PropTypes from 'prop-types'
 import { AboutPageTemplate } from '../../templates/about-page'
 
 const AboutPagePreview = ({ entry, widgetFor }) => (
+
+
   <AboutPageTemplate
     title={entry.getIn(['data', 'title'])}
     content={widgetFor('body')}
-    about={widgetFor('about')}
   />
 )
 
